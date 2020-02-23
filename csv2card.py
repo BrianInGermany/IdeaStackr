ceb3767511366ddc500b7c13fd2bc9cb2429f5fe
Brian Junker-Latocha <b.k.junker@gmail.com>
2020-02-23 16:42:07 +0100

    filled in some python code

--- csv2card.py
+++ csv2card.py
@@ -1,5 +1,23 @@
 import imgkit
 import sys, os
+import csv
 
-# this will be the script to generate cards from CSV 
-# also it will validate the CSV first
+# WIP! Does not work yet!
+csvPath = input("Please enter path of CSV file:\n")
+imgModulePath = input("Please enter path to 'wkhtmltoimg' executable:\n")
+csvFile = open(csvPath, "r", encoding="utf-8")
+csvReader = csv.reader(csvFile, delimiter=";")
+def validate_csv(csvReader):
+    rowIndex = 0
+    for row in csvReader:
+        rowIndex += 1
+        if rowIndex > 1:
+            pass
+def save_img_cards(csvReader):
+    rowIndex = 0
+    for row in csvReader:
+        rowIndex += 1
+        if rowIndex > 1:
+
+            # config = imgkit.config(wkhtmltoimage='/examplePath/bin/wkhtmltoimage')
+            config = imgkit.config(wkhtmltoimage=imgModulePath)
